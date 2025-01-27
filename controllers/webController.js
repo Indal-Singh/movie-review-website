9d1f54166c927be71b5117453cec82ef7c694884
Indal Singh <ind9546@gmail.com>
2025-01-27 07:48:26 +0530

    fixed title

--- controllers/webController.js
+++ controllers/webController.js
@@ -15,19 +15,21 @@ module.exports.webHome = (req, res) => {
               movie.parseContentHtml = movie.description.replace(/<[^>]*>?/gm, '').substring(0, 100);
               return movie;
          });
-       res.render('tamplate/layout',{body:'home',movies:movies});
+       res.render('tamplate/layout',{body:'home',movies:movies,title:'Home'});
       });
 };
 
 module.exports.webPost = (req, res) => {
     const id = req.params.id;
+    let title = "Post Title";
     Web.getMovieById(id,(err, movie) => {
         if (err) {
             console.error('Error fetching movie:', err);
             res.redirect('/');
         }
+        title = movie.title;
         movie.release_date = utilFunctiion.formatDatae(movie.release_date);
         movie.url = '/post/' + movie.id+ '/' + slugify(movie.title.toLowerCase());
-        res.render('tamplate/layout',{body:'postdetails',movie:movie});
+        res.render('tamplate/layout',{body:'postdetails',movie:movie,title:title});
     });
 }
